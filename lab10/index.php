a53176af13e080c65136180927c4301ce55af8e4
Neeraja444 <69445988+Neeraja444@users.noreply.github.com>
2021-11-14 16:01:41 +0530

    Add files via upload

---
+++ lab10/index.php
@@ -0,0 +1,63 @@
+<?php
+include 'connect.php';
+if(isset($_POST['submit'])){
+  $name=$_POST['name'];
+  $email=$_POST['email'];
+  $mobile=$_POST['mobile'];
+  $password=$_POST['password'];
+
+$sql="INSERT INTO `crud`(name, email, mobile, password) 
+values ('$name','$email','$mobile','$password')";
+$result=mysqli_query($con,$sql);
+if($result){
+  #echo "data inserted succefully";
+  header("location:display.php");
+}else{
+  die(mysqli_error($con));
+}
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    <title>Crud Operation</title>
+  </head>
+  <body>
+  <div class = "container">
+  <form method="post">
+  <div class="form-group">
+    <label>Enter Name </label>
+    <input type="text" class="form-control"  placeholder="studentEnter your name " name="name"
+    name="name" autocomplete="off">
+  </div>
+  <div class="form-group">
+    <label>Enter Email </label>
+    <input type="email" class="form-control"  placeholder="studentEnter your email " name="email"
+    name="email" autocomplete="off">
+  </div>
+  <div class="form-group">
+    <label>Enter Mobile Number </label>
+    <input type="text" class="form-control"  placeholder="studentEnter your mobile Number " name="mobile"
+    name="mobile" autocomplete="off">
+  </div>
+  <div class="form-group">
+    <label>Enter Password </label>
+    <input type="text" class="form-control"  placeholder="studentEnter your password " name="password"
+    name="password" autocomplete="off">
+  </div>
+  
+  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+</form>
+    </div>
+
+   
+  </body>
+</html>
